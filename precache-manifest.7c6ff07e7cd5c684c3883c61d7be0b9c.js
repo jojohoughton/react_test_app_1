d1985d3f958f398ec5fb52d1e696d23d85b1f9d9
jojo houghton <jojohoughton1@jojos-MacBook-Pro.local>
2019-02-21 12:28:34 -0500

    Updates

---
+++ precache-manifest.7c6ff07e7cd5c684c3883c61d7be0b9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "906544af4a1c57545fdd",
+    "url": "/jojohoughton/react_test_app_1/static/css/main.be9fecb8.chunk.css"
+  },
+  {
+    "revision": "906544af4a1c57545fdd",
+    "url": "/jojohoughton/react_test_app_1/static/js/main.906544af.chunk.js"
+  },
+  {
+    "revision": "6b507bc85e104d4bf15b",
+    "url": "/jojohoughton/react_test_app_1/static/js/runtime~main.6b507bc8.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/jojohoughton/react_test_app_1/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "adc633b7276da8e12a55d76cf1c62e3b",
+    "url": "/jojohoughton/react_test_app_1/index.html"
+  }
+];
